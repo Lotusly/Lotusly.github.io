8eaa215c4792c084e0087e8e11be981f16861164
Lotusly <yul4@andrew.cmu.edu>
2019-09-08 22:31:26 -0700

    Make background black(use style tag). Remove footer (for its white edge). Fixed one vulnerability

--- Comic/BeiTouNvWu/ImgArray.js
+++ Comic/BeiTouNvWu/ImgArray.js
@@ -1,14 +1,18 @@
 (function ($) {
 
-    var reg_path = new RegExp("(^|&)chapter=([^&]*)(&|$)");
+    var reg_path = new RegExp("(^|&)chapter=([^&]+)(&|$)");
     var url = window.location.search.substr(1).match(reg_path);
-    var chapter=unescape(url[2]);
-    var i=1;
+    if(url){
+	    var chapter=unescape(url[2]);
+	    var i=1;
 
-    var body = document.getElementById("div1");
-    var id = "img_1";
-    body.innerHTML=' <center> <h2 style="padding:50px"> Chapter ' +chapter +'</h2></center>\n';
-    addImg(chapter,1);
+	    var body = document.getElementById("div1");
+	    var id = "img_1";
+	    body.innerHTML='<img id="img_1" \
+				src="images/'+chapter+'/1.jpg" \
+				onerror="finalError(1)" \
+				onload="addImg('+chapter+','+'2)"/>\n';
+	}
 
 })(jQuery);
 
@@ -18,6 +22,7 @@ function addImg(chapter, i){
 	
 	var body = document.getElementById("div1");
 	var thisImg = document.getElementById("img_"+i);
+    		
 
 	if(!thisImg){
 
@@ -27,6 +32,17 @@ function addImg(chapter, i){
 			lastImg.setAttribute("onload","");
 			lastImg.setAttribute("onerror","this.hidden=true");
 		}
+
+
+		if(body.firstChild.tagName.toUpperCase()=="IMG"){
+
+			var h2 = document.createElement('h2');
+			h2.setAttribute("style","padding:50px");
+			h2.innerHTML="Chapter "+chapter;
+    		
+			body.insertBefore(h2, body.firstChild);
+		}
+
 		
 		var img = document.createElement('img');
 		img.setAttribute("id","img_"+i);
@@ -34,9 +50,7 @@ function addImg(chapter, i){
 		img.setAttribute("onload",'addImg('+chapter+','+(i+1)+')');
 		img.setAttribute("onerror",'finalError('+i+')');
 
-		var center = document.createElement('center');
-		center.appendChild(img);
-		body.appendChild(center);
+		body.appendChild(img);
 	}
 	
 };
@@ -53,13 +67,13 @@ function finalError(i){
 				lastImg.setAttribute("onload","");
 				lastImg.setAttribute("onerror","this.hidden=true");
 			}
-			body.removeChild(thisImg.parentElement);
+			body.removeChild(thisImg);
 		}
 	}
 	else{ //no picture at all
 		if(thisImg) {
-			body.removeChild(thisImg.parentElement);
-			body.innerHTML="<center><h2>Chapter doesn't exist</h2></center>";
+			body.removeChild(thisImg);
+			body.innerHTML="<h2>Chapter doesn't exist</h2>";
 		}
 
 	}
